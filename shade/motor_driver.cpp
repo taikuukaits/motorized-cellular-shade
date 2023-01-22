8cea80faec174f11fed542e4f30aeac153e2c9a0
jordan_piscitelli <jordan.r.piscitelli@gmail.com>
2023-01-21 19:25:30 -0500

    refactor: Moving motor code into the original motor driver class.

--- shade/motor_driver.cpp
+++ shade/motor_driver.cpp
@@ -1,71 +1,140 @@
 #include "motor_driver.h"
 #include "Arduino.h"
+#include <Encoder.h>
 
-MotorDriver::MotorDriver(int pinA, int pinB, int pinEncoder){
-  _pinA = pinA;
-  _pinB = pinB;
-  _pinEncoder = pinEncoder;
+MotorDriver::MotorDriver(int motor_pin_one, int motor_pin_two, int encoder_pin_one, int encoder_pin_two){
+  _motor_pin_one = motor_pin_one;
+  _motor_pin_two = motor_pin_two;
+  _encoder = Encoder(encoder_pin_one, encoder_pin_two);
 }
 
-void MotorDriver::begin() {
-    pinMode(_pinEncoder, INPUT_PULLUP);
-    pinMode(_pinA, OUTPUT);
-    pinMode(_pinB, OUTPUT);
+void MotorDriver::setup() {
+    pinMode(_motor_pin_one, OUTPUT);
+    pinMode(_motor_pin_two, OUTPUT);
+    _halt();
+}
 
-    motor_stop();
+void MotorDriver::request_move() {
+    if (_moving) {
+        return false; 
+    }
+    _moving = true;
+    _reached_target = false;
+    _target = _new_target;
+
+    if (_new_target > _position) {
+        _direction_positive = true; 
+    } else {
+        _direction_positive = false;
+    }
+
+    if (_direction_positive) {
+        _move_positive();
+    } else {
+        _move_negative();
+    }
 }
 
 void MotorDriver::loop() {
-    if (_steps <= 0) {
+  int _next_position = encoder.read();
+  if (_next_position != _current_position) {
+      _current_position = next_position;
+      _current_position_changed = true;
+      _millis_when_position_last_changed = millis();
+      _diagnostic_position_change();
+  } else {
+      _current_position_changed = false;
+  }
 
-      if (_motor_on) {
-        motor_stop();
+  if (_current_position_changed) {
+      if (_moving && !_reached_target) {
+          if (_has_reached_target()) {
+              _reached_target = true;
+              _millis_when_reached_target = millis();
+              _diagnostic_reached_target();
+              _halt();
+          }
+      } else if (_moving && reached_target && is_within_reached_target_delay()) {
+          // normal situation, this is totally fine, we just swallow this movement
+          _diagnostic_position_change_detected_within_delay();
+      } else {
+          //the panic situation - we were not moving or are outside of the delay (which shouldn't be possible)
+          _diagnostic_position_change_without_movement();
+          storageSaveCurrentPosition(_current_position); 
       }
-      return;
-    }
+  } else if (_moving) {
+      // if we are trying to move but the last change was too long ago, assume something has gone wrong
+      if (_has_stall_delay_been_exceeded()){
+          _diagnostic_motor_stall_detected();
+          _moving = false; //stop moving, we haven't reached the target but nothing else to be done.
+          _halt();
+      }
+  }
 
-    int currMillis = millis();
-    if (currMillis - _lastEncoding > 50) {
-      _lastEncoding = currMillis;
-    }else{
-      return;
-    }
-    bool encoder_on = digitalRead(_pinEncoder);
-    if (encoder_on && !_tripped) {
-        _tripped = true;
-        _steps--;
-    } else if (!encoder_on) {
-        _tripped = false;
-    }
+  // Even if the position did not change - but we are moving, and at the target, when the delay is exceeded, we are dont moving
+  if (_moving && _reached_target && !_is_within_reached_target_delay()) {
+      _moving = false;
+      _reached_target = true;
+  }
 }
 
 bool MotorDriver::is_moving() {
-  return _motor_on;
+    return _moving;
 }
 
-void MotorDriver::move_forward_steps(int steps) {
-    _steps = steps;
-    _motor_on = true;
-    digitalWrite(_pinA, HIGH);
-    digitalWrite(_pinB, LOW);
-    Serial.println("Forward!");
-
+bool MotorDriver::_has_reached_target() {
+    if (_direction_positive) {
+        if (_current_position > _target) {
+            return true;
+        }
+    } else {
+        if (_current_position < _target) {
+            return true;
+        }
+    }
+    return false;
 }
 
-void MotorDriver::move_backward_steps(int steps) {
-    _steps = steps;
-    _motor_on = true;
-    digitalWrite(_pinA, LOW);
-    digitalWrite(_pinB, HIGH);
-    Serial.println("Backward!");
-
+void MotorDriver::_move_positive() {
+  digitalWrite(_motor_pin_one, LOW);
+  digitalWrite(_motor_pin_two, HIGH);
 }
 
-void MotorDriver::motor_stop() {
-    _steps = 0;
-    _motor_on = false;
-    digitalWrite(_pinA, LOW);
-    digitalWrite(_pinB, LOW);
-    Serial.println("Stop!");
+void MotorDriver::_move_negative() {
+  digitalWrite(_motor_pin_one, HIGH);
+  digitalWrite(_motor_pin_two, LOW);
+}
 
+void MotorDriver::_halt() {
+  digitalWrite(_motor_pin_one, LOW);
+  digitalWrite(_motor_pin_two, LOW);
+}
+
+void MotorDriver::_has_stall_delay_been_exceeded() {
+    return (unsigned long)(millis() - millis_when_position_last_changed) > position_change_stall_delay;
+}
+
+void MotorDriver::_is_within_reached_target_delay() {
+    return (unsigned long)(millis() - millis_when_reached_target) > reached_target_delay;
+}
+
+void MotorDriver::_diagnostic_position_change_without_movement() {
+    Serial.println("ERROR: Detected position change after delay! Something could be wrong.");
+}
+
+void MotorDriver::_diagnostic_position_change_detected_within_delay() {
+    Serial.println("trace: Normal position change between delay and finish moving.");
+}
+
+void MotorDriver::_diagnostic_motor_stall_detected() {
+    Serial.println("ERROR: Motor stall detected! Halting movement.");
+}
+
+void MotorDriver::_diagnostic_position_change() {
+    Serial.print("Position changed: ");
+    Serial.println(_current_position);
+}
+
+void MotorDriver::_diagnostic_reached_target() {
+    Serial.print("Reached target!");
 }
--- shade/motor_driver.h
+++ shade/motor_driver.h
@@ -1,7 +1,7 @@
 class MotorDriver 
 {
     public:
-        MotorDriver(int motorPinPositive, int motorPinNegative, int encoderPinOne, int encoderPinTwo);
+        MotorDriver(int motor_pin_one, int motor_pin_two, int encoder_pin_one, int encoder_pin_two);
 
         void setup(); // The position always starts at 0 after setup. The position is read-only. 
         void loop();
@@ -13,9 +13,21 @@ class MotorDriver
         void set_position_changed_callback(void (*callback)(int)); 
 
     private: 
-        void motor_stop();
+        void _has_reached_target();
+        void _move_positive();
+        void _move_negative();
+        void _halt();
+        bool _has_stall_delay_been_exceeded();
+        bool _is_within_reached_target_delay();
+        void _diagnostic_position_change_without_movement();
+        void _diagnostic_position_change_detected_within_delay();
+        void _diagnostic_motor_stall_detected();
+        void _diagnostic_position_change();
+        void _diagnostic_reached_target();
+
         Encoder encoder;
-        
+        bool _moving = false;
+
         int _target_position = 0; // only valid if moving
         int _current_position = 0; // always valid, relative to encoder startup. starts at zero from power up.
         bool _direction_positive = false; // only valid if moving, target is in the positive direction or negative direction
@@ -27,4 +39,8 @@ class MotorDriver
         int _position_change_stall_delay = 50; // how long to wait before assuming motor has stalled
 
         bool _current_position_changed = false;
+
+        int _motor_pin_one;
+        int _motor_pin_two;
+        Encoder encoder;
 };

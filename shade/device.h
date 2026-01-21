73f4549584ad110f487c68641eab5e00edffe357
jordan_piscitelli <jordan.r.piscitelli@gmail.com>
2026-01-20 23:38:02 -0500

    purging irrelevant files

--- shade/device.h
+++
@@ -1,8 +0,0 @@
-#define DEVICE_ID "prototype-window-1"
-#define DEVICE_NAME "Prototype Window 1"
-
-#define MOTOR_PIN_ONE 16 // D0
-#define MOTOR_PIN_TWO 13 // D7
-
-#define ENCODER_PIN_ONE 14 // D5 - interrupt safe, see https://blog.hiebl.cc/posts/wemos-d1-mini-pro-digital-interrupt-pins/ - CLK pin
-#define ENCODER_PIN_TWO 12 // D6 - interrupt safe - DT
